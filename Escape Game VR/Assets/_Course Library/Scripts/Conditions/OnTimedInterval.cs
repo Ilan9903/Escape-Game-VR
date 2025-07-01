13d7b945b86ea8ca8b45b3c9771afbcfb7abd9cc
Ilan ARFI <i.arfipro@outlook.fr>
2025-07-01 23:09:51 +0200

    maj

--- Escape Game VR/Assets/_Course Library/Scripts/Conditions/OnTimedInterval.cs
+++ Escape Game VR/Assets/_Course Library/Scripts/Conditions/OnTimedInterval.cs
@@ -1,37 +1,37 @@
-﻿using System.Collections;
-using UnityEngine;
-using UnityEngine.Events;
+﻿//using System.Collections;
+//using UnityEngine;
+//using UnityEngine.Events;
 
-/// <summary>
-/// Within a range in seconds, call an event, continues for lifetime of object.
-/// </summary>
-public class OnTimedInterval : MonoBehaviour
-{
-    [Tooltip("The minimum range")]
-    public float minInterval = 0.0f;
+///// <summary>
+///// Within a range in seconds, call an event, continues for lifetime of object.
+///// </summary>
+//public class OnTimedInterval : MonoBehaviour
+//{
+//    [Tooltip("The minimum range")]
+//    public float minInterval = 0.0f;
 
-    [Tooltip("The maximum range")]
-    public float maxInterval = 1.0f;
+//    [Tooltip("The maximum range")]
+//    public float maxInterval = 1.0f;
 
-    // Called once the wait is over
-    public UnityEvent OnIntervalElapsed = new UnityEvent();
+//    // Called once the wait is over
+//    public UnityEvent OnIntervalElapsed = new UnityEvent();
 
-    private void Start()
-    {
-        StartCoroutine(IntervalRoutine());
-    }
+//    private void Start()
+//    {
+//        StartCoroutine(IntervalRoutine());
+//    }
 
-    private IEnumerator IntervalRoutine()
-    {
-        float interval = Random.Range(minInterval, maxInterval);
-        yield return new WaitForSeconds(interval);
+//    private IEnumerator IntervalRoutine()
+//    {
+//        float interval = Random.Range(minInterval, maxInterval);
+//        yield return new WaitForSeconds(interval);
 
-        PlayEvent();
-        StartCoroutine(IntervalRoutine());
-    }
+//        PlayEvent();
+//        StartCoroutine(IntervalRoutine());
+//    }
 
-    private void PlayEvent()
-    {
-        OnIntervalElapsed.Invoke();
-    }
-}
+//    private void PlayEvent()
+//    {
+//        OnIntervalElapsed.Invoke();
+//    }
+//}
